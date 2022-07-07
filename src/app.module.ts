733eedee18b97bcbb531409e0665d6bd10555964
Benja Sarría Ferrer <benja.sarria@live.com.ar>
2022-07-06 22:38:36 -0300

    (Feature) | Se realizaron correcciones para correcta implementacion de framework nestjs con graphql

---
+++ src/app.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ItemsModule } from './items/items.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot({
+      autoSchemaFile: 'schema.gql',
+      driver: ApolloDriver,
+    }),
+    ItemsModule,
+    MongooseModule.forRoot('mongodb://localhost/nestgraphql'),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
